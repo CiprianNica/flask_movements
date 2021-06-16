002313b8550a3f2d3a32e176f13ac263c6740b12
CiprianNica <ciprian.nica77@gmail.com>
2021-06-16 22:00:18 +0200

    Presentacion CSS y HTML

---
+++ movements/views.py
@@ -0,0 +1,11 @@
+from re import template
+from movements import app
+from flask import render_template
+
+@app.route('/')
+def listaMovimientos():
+    return render_template('movementsList.html', miTexto="Ya veremos si hay lista")
+
+@app.route('/creaalta')
+def creaAlta():
+    return 'Tengo que devolver una lista de movimientos.'
