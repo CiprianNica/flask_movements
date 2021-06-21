eb306a7f5bf542b905094568c4332241efcbfa82
CiprianNica <ciprian.nica77@gmail.com>
2021-06-21 22:16:44 +0200

    formularios

--- movements/views.py
+++ movements/views.py
@@ -1,11 +1,21 @@
-from re import template
+from flask.scaffold import F
 from movements import app
 from flask import render_template
+import csv
 
 @app.route('/')
-def listaMovimientos():
-    return render_template('movementsList.html', miTexto="Ya veremos si hay lista", texto='hola')
+def listaIngresos():
+    fIngresos = open("movements/data/basededatos.csv", "r")
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
+    ingresos =list(csvReader)
+    sumador = 0
+    for ingreso in ingresos:
+        sumador += float(ingreso[2])
+    
+    print(ingresos)
+
+    return render_template('movementsList.html', datos=ingresos, total=sumador)
 
 @app.route('/creaalta')
-def creaAlta():
-    return 'Tengo que devolver una lista de movimientos.'
+def nuevoIngreso():
+    return 'Ya luego si eso te enseño el formulario.'
